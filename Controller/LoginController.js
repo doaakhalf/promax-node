cebb3701d6d95a67da7124fb581e7fc305da0b9f
doaakhalf <khalf_doaa@yahoo.com>
2026-07-05 21:12:24 +0300

    add log body

--- Controller/LoginController.js
+++ Controller/LoginController.js
@@ -93,6 +93,7 @@ export default async function LoginController(req, res) {
 export async function EditCoachProfile(req, res) {
     try {
         const body = req.body;
+        console.log(body);
         const user_type=req.user.role_id.name;
         const userUpdate= {}
         if (body.firstName) userUpdate.firstName = body.firstName;
