956e56fe37b4f7d99b8212845ef7cd41f1a0f0f4
doaakhalf <khalf_doaa@yahoo.com>
2026-09-06 21:43:21 +0300

    add exercise create error log

--- Controller/ExerciseController.js
+++ Controller/ExerciseController.js
@@ -27,12 +27,12 @@ export const  create=async(req, res,next)=> {
                   });
             }
             catch(err){
-                logger.error(err.message(),err);
+                console.log(err.message(),err);
                 res.status(500).json({ message: "Failed to create exercise", error: err?.message });
             }
         
   } catch (err) {
-    logger.error(err.message(),err);
+    console.log(err.message(),err);
     res.status(500).json({ message: "Failed to create exercise", error: err?.message });
   }
  
